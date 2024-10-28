f7ffdfb8ee70dae272d3d6ece5d406b1a55cfe66
Oliver <olive@OPODesktop>
2024-10-28 22:51:14 +1100

    attempt at adding read disk

--- boot.asm
+++ boot.asm
@@ -1,25 +1,35 @@
 [org 0x7c00]
+[bits 16]
 
-	mov ah , 0x0e
-	mov bp, 0x8000
+	; VGA mode
+	;mov ax, 0x13
+	;int 0x10
+	
+	mov bp, 0x7c00
 	mov sp, bp
-
-	mov bx, 'b'
-	mov al, bl
-	int 0x10
 
 	mov bx, msg
 	call print
 
-	mov bx, 'L'
-	mov al, bl
-	int 0x10
+	mov bx, 0x7e00 ; load 0x000
+	mov dh, 1
+	mov dl, [BOOT_DRIVE]
+	call disk_load
 
 	jmp $
 
 msg: db "Baby gronk needs his milk", 13, 10, 0
 
+; Globals
+
+BOOT_DRIVE: db 0
+
+; IMPORT HERE
+
 %include "lib/io.asm"
 
 times 510-($-$$) db 0 ; make it 512 bytes so pc knows it's a boot sector
 dw 0xaa55 ; magic number
+
+times 256 dw 0xdada
+times 256 dw 0xface
--- lib/io.asm
+++ lib/io.asm
@@ -5,22 +5,61 @@
 ; 
 
 print:
-  pusha
+  push bx
+  ; set text output
+  mov ah, 0x0e
 
 print_loop:
   
-  mov cx, [bx]
-
-  cmp cl, 0
+  mov al, [bx]
+  test al, al
   je print_loop_end
 
-  mov al, cl
   int 0x10
-
   inc bx
   jmp print_loop
 
 print_loop_end:
-
-  popa
+  pop bx
   ret
+
+;
+; disk_load
+; load sectors specified by dh
+; Params:
+; dh - amount of sectors to load
+; bx - limit
+;
+disk_load:
+  push dx
+  
+
+  ; also its up to user to set bx (load limit)
+  mov ah, 0x02 ; BIOS read sector
+  mov al, dh ; Read dh sectors
+  mov ch, 0x00 ; select cylinder 0
+  mov dh, 0x00 ; select head 0
+  mov cl, 0x02 ; start reading from 2nd sector (after boot sector)
+  int 0x13
+
+  jc disk_err_1 ; jmp if carry flag is set
+  ; carry flag is used as general error
+
+  cmp dh, al ; if sectors read != sectors expected then error
+  jne disk_err_2
+  
+  pop dx
+  ret
+
+disk_err_1:
+  mov bx, disk_err_1_msg
+  jmp print_disk_err
+disk_err_2:
+  mov bx, disk_err_2_msg
+
+print_disk_err:
+  call print
+  jmp $
+
+disk_err_1_msg: db "Disk read error 1!", 0
+disk_err_2_msg: db "Disk read error 2!", 0
