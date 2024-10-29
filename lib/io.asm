52e18505018462fe7a7285919432bcc3fdceae83
Oliver <olive@OPODesktop>
2024-10-30 01:27:55 +1100

    got a20 working

--- lib/io.asm
+++ lib/io.asm
@@ -59,9 +59,11 @@ disk_err_2_msg: db "Disk read error 2!", 0
 
 enable_a20:
   pusha
+  
   mov ax, 0x2401 ; Fast a20
   int 0x15
   jc a2_err
+
   popa
   ret
 
@@ -75,15 +77,15 @@ a2_err_msg: db "A2 error!", 0
 test_a20:
   pusha
 
-  mov ax, 0x0
+  mov ax, 0x0000
   mov es, ax
   mov byte [es:0x000000], 0x55
-  mov byte [es:0x100000], 0xAA
+  mov byte [es:0x000010], 0xAA
   
   cmp byte [es:0x000000], 0x55
   jne a2_test_err
 
-  cmp byte [es:0x100000], 0xAA
+  cmp byte [es:0x000010], 0xAA
   jne a2_test_err
 
   mov bx, a2_success_msg
